190fd9f1defdd20340af5efa0ad380d074cccd11
saisandhy <115157240+saisandhy@users.noreply.github.com>
2022-10-19 17:36:43 +0530

    Add files via upload

---
+++ CoreAssignment/Program.cs
@@ -0,0 +1,18 @@
+﻿using CoreData;
+using CoreEntity;
+using CorePresentation;
+using System;
+
+namespace CoreAssignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+
+        { 
+          
+            MoviePL obj1 = new MoviePL();
+           obj1.Menu();
+        }
+    }
+}
