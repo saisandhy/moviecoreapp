935efaea2c7323195038226d910d59f3823641e5
saisandhy <115157240+saisandhy@users.noreply.github.com>
2022-10-19 17:40:22 +0530

    Add files via upload

---
+++ CorePresentation/MoviePL.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using CoreEntity;
+using CoreData;
+using System.Collections.Generic;
+
+namespace CorePresentation
+{
+    public class MoviePL
+    {
+        public void Menu()
+        {
+            Console.WriteLine("Againnnnnn........");
+            Console.WriteLine("enter 1 to add");
+            Console.WriteLine("enter 2 to delete");
+            int x = Convert.ToInt32(Console.ReadLine());
+            if(x == 1)
+            {
+                AddMovie();
+            }
+            else if (x == 2)
+            {
+                RemoveMovie();
+            }
+        }
+        
+        public void AddMovie()
+        {
+            MovieDAL movieOperations = new MovieDAL();
+            Movie movie = new Movie();
+            Console.Write("Enter movie name:");
+            movie.Name = Console.ReadLine();
+            Console.WriteLine("Enter Movie Description:");
+            movie.MovieDesc = Console.ReadLine();
+            Console.WriteLine("enter movie type:");
+            movie.MovieType = Console.ReadLine();
+            string msg = movieOperations.AddMovie(movie);
+            Console.WriteLine(msg);
+            Menu();
+        }
+        public void RemoveMovie()
+        {
+
+            MovieDAL movieOperations = new MovieDAL();
+            Movie movie = new Movie();
+            Console.Write("enter the Mvie ID :");
+            var id= Convert.ToInt32(Console.ReadLine());
+            string msg= movieOperations.DeleteMovie(id);
+            Console.WriteLine(msg);
+            Menu();
+
+        }
+        public void ShowAllMovies()
+        {
+            MovieDAL movieOperations = new MovieDAL();
+            List<Movie> movies = movieOperations.ShowAll();
+            foreach (var movie in movies)
+            {
+                Console.WriteLine("NAME: " + movie.Name);
+                Console.WriteLine("DESCRIPTION: " + movie.MovieDesc);
+                Console.WriteLine("MovieType: " + movie.MovieType);
+            }
+        }
+    }
+}
---
+++ CorePresentation/TheaterPL.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CoreData;
+using CoreEntity;
+
+namespace CorePresentation
+{
+    public  class TheaterPL
+    {
+        public void Tickets()
+        {
+            Console.WriteLine("Againnnnnn........");
+            Console.WriteLine("enter 1 to bookseats");
+            Console.WriteLine("enter 2 to delete");
+            int x = Convert.ToInt32(Console.ReadLine());
+            if (x == 1)
+            {
+                AddTheater();
+            }
+            else if (x == 2)
+            {
+                RemoveTheater();
+            }
+        }
+        public void AddTheater()
+        {
+            TheaterDAL theateroperations = new TheaterDAL();
+            Theater theater = new Theater();
+            Console.Write("Enter  theater  name:");
+            theater.Name = Console.ReadLine();
+            Console.WriteLine("Enter theater Address:");
+            theater.address = Console.ReadLine();
+            Console.WriteLine("Enter comments:");
+            theater.comments = Console.ReadLine();
+            string msg = theateroperations.AddTheater(theater);
+            Console.WriteLine(msg);
+            Tickets();
+
+        }
+        public void RemoveTheater()
+        {
+
+        }
+        
+
+    }
+}
